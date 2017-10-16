5f046804f96d36f2603d55c85b90cd8b4b83572d
Matthew Marcotte <marcotte.m@husky.neu.edu>
2017-10-16 19:40:48 -0400

    better website-new

--- src/app/components/website/website-new/website-new.component.ts
+++ src/app/components/website/website-new/website-new.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {WebsiteService} from '../../../services/website.service.client';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-website-new',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WebsiteNewComponent implements OnInit {
 
-  constructor() { }
+  userId: String;
+  websites: {};
+  constructor(private websiteService: WebsiteService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRoute.params
+      .subscribe(
+        (params: any) => {
+          this.userId = params['uid'];
+        }
+      );
+    this.websites = this.websiteService.findWebsitesByUser(this.userId);
   }
 
 }
