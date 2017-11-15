be88badeebbfc6cd24f211a2f11bcbb45cf7b1c5
Matthew Marcotte <marcotte.m@husky.neu.edu>
2017-11-15 17:19:22 -0500

    gym/store lists not working yet

--- src/app/components/project-posts/gyms/gyms.component.ts
+++ src/app/components/project-posts/gyms/gyms.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import {NgForm} from '@angular/forms';
-import { Router } from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {GymService} from '../../../services/project.gym.service.client';
+import {PersonService} from "../../../services/project.user.service.client";
 
 @Component({
   selector: 'app-gyms',
@@ -10,31 +11,93 @@ import {GymService} from '../../../services/project.gym.service.client';
 })
 export class GymsComponent implements OnInit {
   @ViewChild('f') searchForm: NgForm;
-  gymInfo: String;
-  gyms;
-  name: String;
-  type: String;
-  address: String;
 
-  constructor(private router: Router, private gymService: GymService) { }
+  userId: String;
+  username: String; email: String; password: String; name: String; role: String;
+  rating: number; followedUsers: [{}]; followedByUsers: [{}]; followedDiets: [{}];
+  followedWorkouts: [{}]; gymMemberships: [{}]; storeMemberships: [{}];
+  allGyms;
+  gymName: String;
+  gymType: String;
+
+  constructor(private router: Router, private gymService: GymService,
+              private activatedRoute: ActivatedRoute,  private accountService: PersonService) { }
 
   ngOnInit() {
+    this.activatedRoute.params
+      .subscribe(
+        (params: any) => {
+          this.userId = params['uid'];
+        });
+    this.gymService.getAllGyms().subscribe(
+      (data: any) => {
+        this.allGyms = data;
+      },
+      (error: any) => {
+
+      }
+    );
   }
 
-  searchGym() {
-    this.gymInfo = this.searchForm.value.gym;
-    this.gymService.getGymByName(this.gymInfo).subscribe(
+  searchGymByName() {
+    this.gymName = this.searchForm.value.name;
+    this.gymService.getGymByName(this.gymName).subscribe(
       (data: any) => {
-        this.gyms = data;
-        this.name = data.name;
-        this.type = data.type;
-        this.address = data.address;
+        this.allGyms = data;
+      },
+      (error: any) => {
+
+      }
+    );
+  }
+
+  searchGymByType() {
+    this.gymType = this.searchForm.value.type;
+    this.gymService.getGymByName(this.gymType).subscribe(
+      (data: any) => {
+        this.allGyms = data;
       },
       (error: any) => {
 
       }
     );
 
+  }
+
+
+  addToProfile(id, fn) {
+    this.accountService.gePersonById(this.userId).subscribe(
+      (data: any) => {
+        this.username = data.username;
+        this.email = data.email;
+        this.password = data.password;
+        this.name = data.name;
+        this.role = data.role;
+        this.rating = data.rating;
+        this.followedUsers = data.followedUsers;
+        this.followedByUsers = data.followedByUsers;
+        this.followedDiets = data.followedDiets;
+        this.followedWorkouts = data.followedWorkouts;
+        this.gymMemberships = data.gymMemberships;
+        this.storeMemberships = data.storeMemberships;
+        this.updateProfile(id, fn);
+      },
+      (error: any) => {
+
+      }
+    );
+  }
+
+  updateProfile(id, gn) {
+    this.gymMemberships.push({gymId: id, name: gn} );
+    this.accountService.updateAccount(this.userId, this.username, this.email, this.password, this.name,
+      this.role, this.rating, this.followedUsers, this.followedByUsers, this.followedDiets, this.followedWorkouts,
+      this.gymMemberships, this.storeMemberships).subscribe(
+      (data: any) => {
+        alert('you added that gym to your profile');
+      }, (error: any) => {
+      }
+    );
   }
 
 }
--- src/app/components/project-posts/stores/stores.component.ts
+++ src/app/components/project-posts/stores/stores.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import {NgForm} from '@angular/forms';
-import { Router } from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {StoreService} from '../../../services/project.store.service.client';
+import {PersonService} from "../../../services/project.user.service.client";
 
 @Component({
   selector: 'app-stores',
@@ -11,28 +12,91 @@ import {StoreService} from '../../../services/project.store.service.client';
 export class StoresComponent implements OnInit {
   @ViewChild('f') searchForm: NgForm;
 
+  userId: String;
+  username: String; email: String; password: String; name: String; role: String;
+  rating: number; followedUsers: [{}]; followedByUsers: [{}]; followedDiets: [{}];
+  followedWorkouts: [{}]; gymMemberships: [{}]; storeMemberships: [{}];
+  allStores;
   storeInfo: String;
-  stores;
-  name: String;
   type: String;
   address: String;
-  constructor(private router: Router, private storeService: StoreService) { }
+  storeName: String;
+  constructor(private router: Router, private storeService: StoreService,
+              private activatedRoute: ActivatedRoute, private accountService: PersonService) { }
 
   ngOnInit() {
-  }
-
-  searchStore() {
-    this.storeInfo = this.searchForm.value.store;
-
-    this.storeService.getStoreByName(this.storeInfo).subscribe(
+    this.activatedRoute.params
+      .subscribe(
+        (params: any) => {
+          this.userId = params['uid'];
+        });
+    this.storeService.getStores().subscribe(
       (data: any) => {
-        this.stores = data;
-        this.name = data.name;
-        this.type = data.type;
-        this.address = data.address;
+        this.allStores = data;
       },
       (error: any) => {
 
+      }
+    );
+  }
+
+  searchStoreByName() {
+    this.storeInfo = this.searchForm.value.name;
+
+    this.storeService.getStoreByName(this.storeInfo).subscribe(
+      (data: any) => {
+        this.allStores = data;
+      },
+      (error: any) => {
+
+      }
+    );
+  }
+
+  searchStoreByType() {
+    this.storeInfo = this.searchForm.value.type;
+
+    this.storeService.getStoreByName(this.storeInfo).subscribe(
+      (data: any) => {
+        this.allStores = data;
+      },
+      (error: any) => {
+
+      }
+    );
+  }
+
+  addToProfile(id, fn) {
+    this.accountService.gePersonById(this.userId).subscribe(
+      (data: any) => {
+        this.username = data.username;
+        this.email = data.email;
+        this.password = data.password;
+        this.name = data.name;
+        this.role = data.role;
+        this.rating = data.rating;
+        this.followedUsers = data.followedUsers;
+        this.followedByUsers = data.followedByUsers;
+        this.followedDiets = data.followedDiets;
+        this.followedWorkouts = data.followedWorkouts;
+        this.gymMemberships = data.gymMemberships;
+        this.storeMemberships = data.storeMemberships;
+        this.updateProfile(id, fn);
+      },
+      (error: any) => {
+
+      }
+    );
+  }
+
+  updateProfile(id, sn) {
+    this.storeMemberships.push({storeId: id, name: sn} );
+    this.accountService.updateAccount(this.userId, this.username, this.email, this.password, this.name,
+      this.role, this.rating, this.followedUsers, this.followedByUsers, this.followedDiets, this.followedWorkouts,
+      this.gymMemberships, this.storeMemberships).subscribe(
+      (data: any) => {
+        alert('you added that store to your profile');
+      }, (error: any) => {
       }
     );
   }
